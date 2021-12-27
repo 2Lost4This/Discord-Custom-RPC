33c5a5ff0e25a543f763fa08d2ba9aa6efaa4ace
2Lost4GitHub <91763324+2Lost4This@users.noreply.github.com>
2021-12-27 18:50:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ client.on('ready', () => {
     client.request('SET_ACTIVITY', {
         pid: process.pid,
         activity: {
-            details: "Moderator & Developer",
-            state: "Feel free to dm me",
-            timestamps: {start: Date.now()},
+            details: "Moderator & Developer", //first line on the Status
+            state: "Feel free to dm me", // Second line on the Status
+            timestamps: {start: Date.now()}, //starting timer
             assets: {
-                large_image: "redpanada",
-                large_text: ":^)"
+                large_image: "redpanada", // Go to the Developer Portal, add an image and add the Name here 
+                large_text: ":^)" //the text from the image 
             },
             buttons: [{
-                label: "Discord",
-                url: "https://discord.gg/4Q87pfTWqC"
+                label: "Discord", //Name of the first Button
+                url: "https://discord.gg/4Q87pfTWqC" //link of the first button
             }, {
-                label: "Spotify",
-                url: "https://open.spotify.com/user/x0qw2ktvf9dhl53iv8jbwelky?si=5a3e85f60de94ee2"
+                label: "Spotify", //name of the second button
+                url: "https://open.spotify.com/user/x0qw2ktvf9dhl53iv8jbwelky?si=5a3e85f60de94ee2" //link of the second button
             }]
     
         }
     })
 })
 client.login({
-    clientId: "925121241902366773"
+    clientId: "Your Client ID" // can be found in your Application from the Developer Portal
 }).catch(console.error);
