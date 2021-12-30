905013ce3fe03671b50a1f53651a1cc2b14fee1c
2Lost4GitHub <91763324+2Lost4This@users.noreply.github.com>
2021-12-30 17:18:10 -0500

    Updated Index.js
    Changed some things (added more infos)

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const client = new rpc.Client({
     transport: 'ipc'
 })
 client.on('ready', () => {
-         console.log('RPC started', );
+         console.log('RPC started', ); //shows message if the RPC successfully started
     client.request('SET_ACTIVITY', {
         pid: process.pid,
         activity: {
-            details: "Moderator & Developer",
-            state: "Feel free to dm me",
-            timestamps: {start: Date.now()},
+            details: "Moderator & Developer", // First Line
+            state: "Feel free to dm me", //  Second Line
+            timestamps: {start: Date.now()}, // Starting Timer
             assets: {
-                large_image: "redpanada",
-                large_text: ":^)",
-                small_image: "redpanada",
-                small_text: "Nothing special here"
+                large_image: "redpanada", // name of the large image in the Developer Portal
+                large_text: ":^)",  // Text shwoing on clicking on the large image 
+                small_image: "redpanada", // Text shwoing on clicking on the small image 
+                small_text: "Nothing special here" // name of the small image in the Developer Portal
             },
             buttons: [{
                 label: "Discord", //Name of the first Button
